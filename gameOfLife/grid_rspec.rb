cc1090ccd1e498f59fc8483deadc7ac109aa07aa
Adrian Robles <adrian.mrj.97@gmail.com>
2021-09-02 19:25:19 -0500

    Unit Tests v2
    Co-authored-by: CUBOFIG <hfigueroa@ucol.mx>

--- gameOfLife/grid_rspec.rb
+++ gameOfLife/grid_rspec.rb
@@ -7,17 +7,21 @@ describe Grid do
   let(:grid) { Grid.new }
 
     context 'grid Test' do
+        # Verificación de Columna
         it 'Column verification' do
             expect(grid.column).to eq 10
         end
+        # Verificación de Fila
         it 'Row Verification' do
             expect(grid.row).to eq 6
         end
+        # Chequeo de espacio de Celulas
         it 'Cells Space Verification' do
             number = grid.column * grid.row
             result = grid.matriz.size * grid.matriz[0].size
             expect(number).to eq result
         end
+        # Comparación de Matrices
         it 'New Generation Comparison' do
             gen = grid.next_generation(grid.matriz)
             expect(gen).to eq grid.matriz
